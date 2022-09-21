8837a775f9c64ca001d7dd4ca93118bc7b243c19
Warin Laocharoen (Chue) <mrwarin@gmail.com>
2022-09-21 19:52:06 +0700

    correct typo

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1> Diamond Assigment </h1>
+      <h1> Diamond Assignment </h1>
       <iframe title="lottie-file" src="https://embed.lottiefiles.com/animation/72681" style={{border: 0, width: 300, height: 300}}></iframe>
     </div>
   );
